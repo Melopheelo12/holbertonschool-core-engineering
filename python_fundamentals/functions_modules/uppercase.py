f48ecf32145dbf66adb708368b24783e5c1331e4
Melopheelo12 <ryan12052002@gmail.com>
2026-05-06 11:37:12 +0200

    t2

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for c in str:
         if ord('97') <= ord(c) <= ord('122'):
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:c}".format(ord(c)), end="")
+            c = chr(ord(char) - 32)
+        print("{}".format(c), end="")
+    print()
