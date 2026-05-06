8c337acdeb5eae805dc6df365b28285b85d9ef33
Melopheelo12 <ryan12052002@gmail.com>
2026-05-06 13:50:01 +0200

    t3

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 def pow(a, b):
     result = 1
+    if b < 0:
+        for i in range(-b):
+            result = result * a
+        return 1 / result
     for i in range(b):
-        result *= a
+        result = result * a
     return result
