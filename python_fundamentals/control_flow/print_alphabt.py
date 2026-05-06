5ef9c281882195db2c2c6e0de698a5372e5de66f
Melopheelo12 <ryan12052002@gmail.com>
2026-05-06 11:08:14 +0200

    T2

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python 3
 for letter in range(ord('a'), ord('z') + 1):
-    if char(letter) != 'q' and char(letter) != 'e':
-        print("{}".format(char(letter)), end="")
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
