2219a0c212f8773c8d810320ef48853412a43b03
Melopheelo12 <ryan12052002@gmail.com>
2026-05-06 13:28:56 +0200

    T1

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    result = ""
-    for char in str:
-        if 'a' <= char <= 'z':
-            result += chr(ord(char) - 32)
-        else:
-            result += char
-    print(result)
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
