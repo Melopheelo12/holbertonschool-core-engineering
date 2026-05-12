fb2f1052aa036a9e4f2fd4ec2788c533aa0f3f11
Melopheelo12 <ryan12052002@gmail.com>
2026-05-12 15:34:15 +0200

    t6

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -41,7 +41,7 @@ class Square:
         for _ in range(self.__position[1]):
             print()
         for _ in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+            print("_" * self.__position[0] + "#" * self.__size)
 
     def __str__(self):
         if self.__size == 0:
@@ -50,5 +50,5 @@ class Square:
         for _ in range(self.__position[1]):
             lines.append("")
         for _ in range(self.__size):
-            lines.append(" " * self.__position[0] + "#" * self.__size)
+            lines.append("_" * self.__position[0] + "#" * self.__size)
         return "\n".join(lines)
