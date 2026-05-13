99179ec57f3c79b00510a74ac93135d10afc910e
Melopheelo12 <ryan12052002@gmail.com>
2026-05-13 13:45:25 +0200

    T1

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module for Rectangle"""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validation("width", width)
+        self.integer_validation("height", height)
+        self.__width = width
+        self.__height = height
