4d4891bbaceb838dda3ad917322da72a4210a903
Melopheelo12 <ryan12052002@gmail.com>
2026-05-13 13:46:33 +0200

    T1

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__('base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
+        """instantiate a rectangle"""
         self.integer_validation("width", width)
         self.integer_validation("height", height)
         self.__width = width
