8ce0ec169c4af0abb69b2c6e01e6cbf70720baec
Melopheelo12 <ryan12052002@mail.com>
2026-07-15 00:46:03 +0000

    echo_server.py

---
+++ python_advanced/websockets/echo_server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+
+async def connection_handler(websocket):
+    ...
+
+async def main():
+    ...
+
+if __name__ == "__main__":
+    asyncio.run(main())
